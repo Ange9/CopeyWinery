c8903132a821b025dbbc7c16e19dd853b12159b0
Ange9 <angelica.fallas@gmail.com>
2019-07-23 00:35:54 -0600

    db connection and user login

--- CopeyWinery/App_Start/RouteConfig.cs
+++ CopeyWinery/App_Start/RouteConfig.cs
@@ -16,7 +16,10 @@ namespace CopeyWinery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                    { controller = "Home",
+                    action = "Login",
+                    id = UrlParameter.Optional }
             );
         }
     }
--- CopeyWinery/Controllers/HomeController.cs
+++ CopeyWinery/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CopeyWinery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +8,46 @@ using System.Web.Mvc;
 namespace CopeyWinery.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
+    { 
+
+        public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+
+                using (DB_Entities db = new DB_Entities())
+                {
+                    var obj = db.User.Where(a => a.Name.Equals(objUser.Name) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["ID"] = obj.ID.ToString();
+                        Session["Name"] = obj.Name.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["ID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    
+
+    public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
